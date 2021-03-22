6844fb2fc46bd4196f4d841abae54431ecaea0aa
AurelijaMaz <aurelija.mazeikiene@gmail.com>
2021-03-22 10:25:56 +0200

    feature: postCar endpoint created

--- server/controlers/cars.js
+++ server/controlers/cars.js
@@ -1,19 +1,36 @@
 const CarModel = require('../models/CarModel')
 
-const getCars = (req, res) => {
+const getCars = async (req, res) => {
     try {
 
         const carsFromDB = await CarModel.find();
-        res.status(200).json({cars: carsFromDB})
+        res.status(200).json({ cars: carsFromDB })
     }
-    catch(error){
-        res.status(404).json({message: error.message})
+    catch (error) {
+        res.status(404).json({ message: error.message })
     }
 }
 
 
-const postCar = (req, res) => {
-    res.status(200).json('Ateityje idesiu viena masina')
+const postCar = async (req, res) => {
+    const { brand, model, year, engineVolume } = req.body;
+    try {
+
+        const newCar = await CarModel.create({ brand, model, year, engineVolume });
+        res.status(200).json({
+            car: {
+                id: newCar._id,
+                brand: newCar.brand,
+                model: newCar.model,
+                year: newCar.year,
+                engineValume: newCar.engineVolume
+            }
+        });
+    
+    }
+    catch (error) {
+    res.status(404).json({ message: error.message })
+}
 }
 
 const updateCar = (req, res) => {
